7497a16ecf0367bbcce9aa20bddffae24a88ef45
Plikins <44211005+alphonus@users.noreply.github.com>
2026-04-22 14:30:37 +0000

    Added Api version checks to remove warnings
    Checked it working on OpenRCT v5.0.0.
    minApiVersion and targetApiVersion may be lowered.

--- src/registerPlugin.ts
+++ src/registerPlugin.ts
@@ -5,6 +5,8 @@ import main from './main';
 registerPlugin({
   name: 'Ride Price Manager',
   version: '1.3.2',
+  minApiVersion: 29,
+	targetApiVersion:113,
   authors: ['mgovea', 'Sadret', 'kscheel'],
   type: 'remote',
   licence: 'MIT',
